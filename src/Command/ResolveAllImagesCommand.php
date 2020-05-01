439973b7a4169aa42547ccb775a5ea8aee913a9a
Thomas Talbot <thomas.talbot@sensiolabs.com>
2020-05-01 17:44:42 +0200

    add resolve all images command

---
+++ src/Command/ResolveAllImagesCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Manufacturer;
+use App\Entity\Ship;
+use App\Repository\ManufacturerRepository;
+use App\Repository\ShipRepository;
+use Liip\ImagineBundle\Imagine\Cache\Helper\PathHelper;
+use Liip\ImagineBundle\Service\FilterService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResolveAllImagesCommand extends Command
+{
+    protected static $defaultName = 'app:resolve-all-images';
+
+    private FilterService $filterService;
+    private ShipRepository $shipRepository;
+    private ManufacturerRepository $manufacturerRepository;
+
+    public function __construct(
+        FilterService $filterService,
+        ShipRepository $shipRepository,
+        ManufacturerRepository $manufacturerRepository
+    ) {
+        parent::__construct();
+        $this->filterService = $filterService;
+        $this->shipRepository = $shipRepository;
+        $this->manufacturerRepository = $manufacturerRepository;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Ship[] $ships */
+        $ships = $this->shipRepository->findAll();
+        foreach ($ships as $ship) {
+            if ($ship->getThumbnailPath() !== null) {
+                $url = $this->resolveImage($ship->getThumbnailPath(), 'thumbnails');
+                $output->writeln($url);
+            }
+            if ($ship->getPicturePath() !== null) {
+                $url = $this->resolveImage($ship->getPicturePath(), 'pictures');
+                $output->writeln($url);
+            }
+        }
+
+        /** @var Manufacturer[] $manufacturers */
+        $manufacturers = $this->manufacturerRepository->findAll();
+        foreach ($manufacturers as $manufacturer) {
+            if ($manufacturer->getLogoPath() !== null) {
+                $url = $this->resolveImage($manufacturer->getLogoPath(), 'logos');
+                $output->writeln($url);
+            }
+        }
+        $output->writeln('<info>Done.</info>');
+    }
+
+    private function resolveImage(string $path, string $filter): string
+    {
+        $path = PathHelper::urlPathToFilePath($path);
+
+        return $this->filterService->getUrlOfFilteredImage($path, $filter);
+    }
+}
